44444ab978c65280e21fab18ac3023f816f64f8e
proalamin <pro.alamin247@gmail.com>
2025-01-07 14:53:49 +0600

    variables and data types

---
+++ first-program/02.varibale_and_datatypes.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	// Integer Data Type
+	var id =22323
+	fmt.Println(id)
+
+	age := 22
+	fmt.Println(age)
+
+
+	
+
+	// Float Data Type
+	var salary1 float32
+	var salary2 float64
+  
+	salary1 = 50000.503882901
+	salary2 = 50000.503882901 // can store decimals with greater precision
+  
+	fmt.Println(salary1) 
+	fmt.Println(salary2)
+
+
+
+	// String Data Type
+	var message string
+	message = "Welcome, welcome to new journey"
+	fmt.Println(message)
+
+
+
+	// Boolean Data Type
+	var isValid bool
+	isValid= false
+	fmt.Println(isValid)
+
+	isPossible := true
+	fmt.Println(isPossible)
+
+}
